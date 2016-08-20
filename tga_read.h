a006df4e2e1be87a97d9ae118486adebbce7a914
Hackerspaceship <hackerspace@hackerspace.derp>
2016-08-20 19:24:15 +0200

    - Add a framework for reading TGA files using SDL for file I/O

---
+++ tga_read.h
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+typedef struct __attribute__((packed)) tga_header_t
+{
+    char idlen;
+    char maptype;
+    char typecode;
+    short int colmaporigin;
+    short int colmaplen;
+    char colmapdepth;
+    short int origin_x;
+    short int origin_y;
+    short int width;
+    short int height;
+    char bpp;
+    char imagedesc;
+} tga_header;
+
+typedef struct tga_data_t
+{
+    tga_header* header;
+    void* file_data;
+    void* img_data;
+    size_t size;
+} tga_data;
+
+tga_data* get_texture_data(const char* texfile)
+{
+    SDL_RWops* rw = SDL_RWFromFile(texfile,"r");
+    if(!rw)
+        return 0x0;
+
+    tga_data* tga = malloc(sizeof(tga_data));
+
+    tga->size = SDL_RWsize(rw);
+    tga->file_data = malloc(tga->size);
+    SDL_RWread(rw,tga->file_data,tga->size,tga->size);
+    SDL_RWclose(rw);
+
+    tga->header = (tga_header*)tga->file_data;
+
+    tga->img_data = (char*)tga->file_data + sizeof(tga_header) + tga->header->idlen;
+
+    return tga;
+}
+
+void free_texture_data(tga_data* data)
+{
+    free(data->file_data);
+    free(data);
+}
